4d7083725e1c105d7114c3b45aa752ccb3489997
Alex White <aawhite@revlocal.com>
2021-09-01 13:56:33 -0700

    Added tabs for prebuilt components

--- code.js
+++ code.js
@@ -20,21 +20,49 @@ if (figma.editorType === 'figma') {
             }
         }
         else if (msg.type === "insertAll") {
-            let data = msg.data;
-            const fields = Object.keys(data);
-            for (const field of fields) {
-                const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`));
-                if (pageNodes.length) {
-                    yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
-                    yield figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" });
-                    for (const pageNode of pageNodes) {
-                        pageNode.characters = data[field];
-                    }
-                }
+            setData(msg.data);
+        }
+        else if (msg.type === "insertLeadCard") {
+            try {
+                const leadNode = figma.root.findOne(e => e.type === "COMPONENT" && e.name === "Strategy Center/Lead Card");
+                if (!leadNode)
+                    return;
+                leadNode.createInstance();
+                setData(msg.data);
+            }
+            catch (_a) {
+                console.log("Failed to load 'Strategy Center/Lead Card' component, please ensure it is in the team library");
+            }
+        }
+        else if (msg.type === "insertAccountCard") {
+            try {
+                const leadNode = figma.root.findOne(e => e.type === "COMPONENT" && e.name === "Strategy Center/Account Details Card");
+                if (!leadNode)
+                    return;
+                leadNode.createInstance();
+                setData(msg.data);
+            }
+            catch (_b) {
+                console.log("Failed to load 'Strategy Center/Account Details Card' component, please ensure it is in the team library");
             }
         }
         else {
             figma.closePlugin();
+        }
+    });
+}
+function setData(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const fields = Object.keys(data);
+        for (const field of fields) {
+            const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`));
+            if (pageNodes.length) {
+                yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
+                yield figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" });
+                for (const pageNode of pageNodes) {
+                    pageNode.characters = data[field];
+                }
+            }
         }
     });
 }
--- code.ts
+++ code.ts
@@ -11,22 +11,49 @@ if (figma.editorType === 'figma') {
         }
       }
     } else if (msg.type === "insertAll") {
-      let data = msg.data
-      const fields = Object.keys(data)
-      
-      for (const field of fields) {
-        const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`))
-        if (pageNodes.length) {
-          await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
-          await figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" })
-          for (const pageNode of pageNodes) {
-            (pageNode as TextNode).characters = data[field]
-          }
-        }
+      setData(msg.data)
+    }
+    else if (msg.type === "insertLeadCard") {
+      try {
+        const leadNode = figma.root.findOne(e => e.type === "COMPONENT" && e.name === "Strategy Center/Lead Card")
+        if (!leadNode) return
+
+        (leadNode as ComponentNode).createInstance()
+
+        setData(msg.data)
+      } catch {
+        console.log("Failed to load 'Strategy Center/Lead Card' component, please ensure it is in the team library");
       }
-    } else {
+    }
+    else if (msg.type === "insertAccountCard") {
+      try {
+        const leadNode = figma.root.findOne(e => e.type === "COMPONENT" && e.name === "Strategy Center/Account Details Card")
+        if (!leadNode) return
+
+        (leadNode as ComponentNode).createInstance()
+        setData(msg.data)
+      } catch {
+        console.log("Failed to load 'Strategy Center/Account Details Card' component, please ensure it is in the team library");
+      }
+    }
+    else {
       figma.closePlugin();
     }
 
   };
+}
+
+async function setData(data) {
+  const fields = Object.keys(data)
+
+  for (const field of fields) {
+    const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`))
+    if (pageNodes.length) {
+      await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
+      await figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" })
+      for (const pageNode of pageNodes) {
+        (pageNode as TextNode).characters = data[field]
+      }
+    }
+  }
 }
