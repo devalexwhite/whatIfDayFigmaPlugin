5e5d25e50c8379e0c68f42c5ad1ae3175d01c060
Alex White <aawhite@revlocal.com>
2021-09-01 12:31:22 -0700

    Adding more fake client data

--- code.js
+++ code.js
@@ -11,21 +11,22 @@ if (figma.editorType === 'figma') {
     figma.showUI(__html__);
     figma.ui.resize(300, 500);
     figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-        yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
         if (msg.type === 'insert') {
             for (const node of figma.currentPage.selection) {
                 if (node.type === 'TEXT') {
+                    yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
                     node.characters = msg.field;
                 }
             }
         }
         else if (msg.type === "insertAll") {
-            yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
             let data = msg.data;
             const fields = Object.keys(data);
             for (const field of fields) {
-                const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.includes(field));
+                const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`));
                 if (pageNodes.length) {
+                    yield figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" });
+                    yield figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" });
                     for (const pageNode of pageNodes) {
                         pageNode.characters = data[field];
                     }
--- code.ts
+++ code.ts
@@ -3,21 +3,22 @@ if (figma.editorType === 'figma') {
   figma.ui.resize(300, 500);
 
   figma.ui.onmessage = async msg => {
-    await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
     if (msg.type === 'insert') {
       for (const node of figma.currentPage.selection) {
         if (node.type === 'TEXT') {
+          await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
           node.characters = msg.field
         }
       }
     } else if (msg.type === "insertAll") {
-      await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
       let data = msg.data
       const fields = Object.keys(data)
-
+      
       for (const field of fields) {
-        const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.includes(field))
+        const pageNodes = figma.currentPage.findAll(sceneNode => sceneNode.visible && sceneNode.type === "TEXT" && sceneNode.name.toLowerCase().includes(`field/${field.toLowerCase()}`))
         if (pageNodes.length) {
+          await figma.loadFontAsync({ family: "SF Pro Display", style: "Regular" })
+          await figma.loadFontAsync({ family: "SF Pro Display", style: "Bold" })
           for (const pageNode of pageNodes) {
             (pageNode as TextNode).characters = data[field]
           }
